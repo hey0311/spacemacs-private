b3bcd4c07966b999031d790b55c5aea3a4c094ef
heyong <hey0311@qq.com>
2020-05-22 09:45:46 +0800

    add org-roam org-roam-server

--- init.el
+++ init.el
@@ -110,7 +110,7 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(sicp ssh-agency cnfonts real-auto-save imenu-list org-journal)
+   dotspacemacs-additional-packages '(sicp ssh-agency cnfonts real-auto-save imenu-list org-journal emacsql-sqlite org-roam org-roam-server)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
